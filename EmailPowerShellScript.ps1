115556ac458303661cf7727fdcaceb0f64748ec9
rajat4803 <nightfox4803@gmail.com>
2017-09-04 19:13:30 +0530

    SendMail.h header file and Send Mail PowerShell Script added

---
+++ EmailPowerShellScript.ps1
@@ -0,0 +1,75 @@
+﻿Param( [String]$Att, [String]$Subject, [String]$Body)
+Function Send-EMail
+{
+    Param(
+                   [Parameter(`
+                   Mandatory=$true)]
+           [String]$To,
+                   [Parameter(`
+                   Mandatory=$true)]
+           [String]$From,
+                    [Parameter(`
+                    Mandatory=$true)]
+           [String]$Password,
+                    [Parameter(`
+                    Mandatory=$true)]
+           [String]$Subject,
+                    [Parameter(`
+                    Mandatory=$true)]
+           [String]$Body,
+                    [Parameter(`
+                    Mandatory=$true)]
+           [String]$attachment
+         )
+         
+    try
+    {
+        $Msg = New-Object System.Net.Mail.MailMessage($From, $To, $Subject, $Body)
+        $Srv = "smtp.gmail.com"   
+        if($attachment -ne $null)
+    {
+        try
+        {
+            $Attachments = $attachment -split ("\:\:");
+
+            ForEach($val in $Attachments)
+         {
+             $attch = New-Object System.Net.Mail.Attachment($val)
+             $Msg.Attachments.Add($attch)
+          }
+        }
+        catch
+        {
+            exit 2;
+        }
+        $Client = New-Object Net.Mail.SmtpClient($Srv,587)
+        $Client.EnableSsl = $true
+        $Client.Credentials = New-Object System.Net.NetworkCredential($From.Split("@")[0], $Password);
+        $Client.Send($Msg)
+        Remove-Variable -Name Client
+        Remove-Variable -Name Password
+        exit 7;
+    }                
+                    
+    }
+    catch
+    {
+        exit 3;
+    }
+}
+
+try
+{
+
+    Send-EMail 
+        -attachment $Att 
+        -To "Address of the recipient"
+        -Body $Body
+        -Subject $Subject
+        -password "Password for the email provided Below"
+        -From "Address of the Sender"
+}
+catch
+{
+    exit 4;    
+}
